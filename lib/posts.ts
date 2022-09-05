4856daf98dea42f7d8a3ccc266c9af9c68d6ce7b
Fabio Viscuso <viscuso.fabio@outlook.it>
2022-09-05 22:50:54 +0200

    feat: added dynamic static pages and some deps

--- lib/posts.ts
+++ lib/posts.ts
@@ -1,6 +1,8 @@
 import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
+import { remark } from 'remark';
+import html from 'remark-html';
 
 const postsDirectory = path.join(process.cwd(), 'posts');
 
@@ -29,11 +31,40 @@ export function getSortedPostsData() {
         if (a < b) {
             return 1;
         } else if (a > b) {
-            return -1
+            return -1;
         } else {
-            return 0
+            return 0;
         }
     })
+}
 
+export function getPostsIds() {
+    const fileNames = fs.readdirSync(postsDirectory);
+    return fileNames.map((fileName) => {
+        // returns something like { params: { id: 'pre-rendering' } }
+        // this object is ready to be passed to getStaticProps() where needed
+        return {
+            params: {
+                id: fileName.replace(/\.md$/, '')
+            }
+        }
+    })
+}
 
+export async function getPostData(id) {
+    // step 1: locate file to read
+    const filePath = path.join(postsDirectory, `${id}.md`);
+    // step 2: write file content in a const
+    const fileContents = fs.readFileSync(filePath, 'utf-8');
+    // step 3: parse the md file content with gray-matter
+    const parsedContent = matter(fileContents);
+    // step 4: use remark to convert markdown into html
+    const processedFromRemark = await remark().use(html).process(parsedContent.content);
+    const htmlContent = processedFromRemark.toString();
+    // step 5: return the id along with the parsed content data
+    return {
+        id,
+        htmlContent,
+        ...parsedContent.data
+    };
 }
