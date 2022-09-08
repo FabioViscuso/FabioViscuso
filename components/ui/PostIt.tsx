e6ed00fbd68926549864525a528d2b5d628d560d
Fabio Viscuso <viscuso.fabio@outlook.it>
2022-09-08 22:56:36 +0200

    feat: add a fix for the overflows and removed date from post-it fields

--- components/ui/PostIt.tsx
+++ components/ui/PostIt.tsx
@@ -1,9 +1,9 @@
-export default function Postit({ key, creator, title, date, content, color = '#f9f474' }) {
+export default function Postit({ creator, title, content, color = '#f9f474' }) {
     return (
-        <div className={`font-indieFlower text-2xl flex flex-col gap-2 p-5 bg-[${color}] w-60 h-60 text-slate-800`}>
+        <div className={`font-indieFlower break-words text-2xl flex flex-col gap-2 p-5 bg-[${color}] w-64 h-64 text-slate-800`}>
             <span className="text-3xl">{title}</span>
-            <span>from: {creator}</span>
-            <span>date: {date}</span>
+            <span className="text-xl">from: {creator}</span>
+            <span className="text-sm leading-3">----</span>
             <span className="m-0 h-full max-h-22 overflow-y-auto">{content}</span>
         </div>
     )
