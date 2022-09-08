523dc2712590c86f4fc56e6d306b3e295c36e7fa
Fabio Viscuso <viscuso.fabio@outlook.it>
2022-09-09 00:36:25 +0200

    feat: new look for the new postit form

--- components/ui/PostIt.tsx
+++ components/ui/PostIt.tsx
@@ -1,10 +1,10 @@
 export default function Postit({ creator, title, content, color = '#f9f474' }) {
     return (
-        <div className={`font-indieFlower break-words text-2xl flex flex-col gap-2 p-5 bg-[${color}] w-64 h-64 text-slate-800`}>
-            <span className="text-3xl">{title}</span>
+        <div className={`font-indieFlower break-words flex flex-col gap-2 p-5 bg-[${color}] w-64 h-64 text-slate-800`}>
+            <span className="text-2xl">{title}</span>
             <span className="text-xl">from: {creator}</span>
             <span className="text-sm leading-3">----</span>
-            <span className="m-0 h-full max-h-22 overflow-y-auto">{content}</span>
+            <span className="text-2xl m-0 h-full max-h-22 overflow-y-auto">{content}</span>
         </div>
     )
 }
