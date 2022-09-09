bc3ed9ec8026f588baccf99e881ee55a330e4789
Fabio Viscuso <viscuso.fabio@outlook.it>
2022-09-10 00:50:55 +0200

    fix: fixed popups animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,11 +10,12 @@ export default function Home() {
     const content = document.getElementById(`${targetID}`).innerHTML
     const trimmedContent = content.replace(/\s+/g, '').trim()
     await navigator.clipboard.writeText(trimmedContent);
-    document.getElementById(`${targetID}Popup`).style.opacity = '1'
     document.getElementById(`${targetID}Popup`).style.visibility = 'visible'
     document.getElementById(`${targetID}Popup`).style.top = '-20px'
+    document.getElementById(`${targetID}Popup`).style.opacity = '1'
 
     setTimeout(() => {
+      document.getElementById(`${targetID}Popup`).style.top = '24px'
       document.getElementById(`${targetID}Popup`).style.opacity = '0'
       document.getElementById(`${targetID}Popup`).style.visibility = 'hidden'
     }, 3000);
