25c8b75f55736a94747a1c79cf65c4fd6fcb0b72
Fabio Viscuso <viscuso.fabio@outlook.it>
2022-09-12 00:40:29 +0200

    feat: added translation to add post-it page

--- pages/new-postit/index.tsx
+++ pages/new-postit/index.tsx
@@ -72,7 +72,7 @@ export default function NewPostitForm() {
 
     return (
         <div className='h-full flex flex-col gap-5 justify-center items-center pt-32'>
-            <h1 className='text-5xl main-text-gradient font-semibold text-center'>Add another post-it to the board!</h1>
+            <h1 className='text-5xl main-text-gradient font-semibold text-center leading-relaxed'>{t('form-head')}</h1>
             <form className='flex flex-col gap-5 justify-between items-center mt-10' onSubmit={submitHandler}>
                 <div className={`flex flex-col gap-2 p-5 w-64 h-64 bg-[${formData.colorSelect}] font-indieFlower break-words`}>
                     <input
@@ -82,7 +82,7 @@ export default function NewPostitForm() {
                         maxLength={25}
                         onChange={(e) => inputChangeHandler(e)}
                         className='bg-transparent text-slate-900 text-2xl resize-none'
-                        placeholder='Insert a title'
+                        placeholder={t('form-placehldr-1')}
                     />
                     <input
                         name='creator'
@@ -91,7 +91,7 @@ export default function NewPostitForm() {
                         maxLength={20}
                         onChange={(e) => inputChangeHandler(e)}
                         className='bg-transparent text-slate-900 text-xl'
-                        placeholder='Who are you?'
+                        placeholder={t('form-placehldr-2')}
                     />
                     <span className="text-sm leading-3 text-slate-900">----</span>
                     <textarea
@@ -101,12 +101,12 @@ export default function NewPostitForm() {
                         maxLength={80}
                         onChange={(e) => inputChangeHandler(e)}
                         className='bg-transparent text-2xl text-slate-900 m-0 h-full max-h-22 overflow-y-auto resize-none'
-                        placeholder='Write something!'
+                        placeholder={t('form-placehldr-3')}
                     ></textarea>
                 </div>
 
                 <p className='text-xl'>
-                    Pick a color
+                    {t('form-colorpick')}
                 </p>
                 <fieldset onChange={(e) => inputChangeHandler(e)} className='flex flex-wrap gap-5 md:gap-8 h-10'>
                     <div className='flex flex-col items-center relative'>
@@ -130,7 +130,9 @@ export default function NewPostitForm() {
                         <label htmlFor="option5" className='w-10 h-10 rounded-full bg-[#f97474]'></label>
                     </div>
                 </fieldset>
-                <button className="text-lg bg-gradient-to-bl from-teal-400 to-emerald-400 px-4 py-2 mt-10 rounded-lg text-slate-800 font-semibold">Add this post-it!</button>
+                <button className="text-lg bg-gradient-to-bl from-teal-400 to-emerald-400 px-4 py-2 mt-10 rounded-lg text-slate-800 font-semibold">
+                    {t('form-submit')}
+                </button>
             </form>
         </div >
     );
