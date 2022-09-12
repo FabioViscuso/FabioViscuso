b39e6af3af71ce7ffb7c83baa5ac0ee11db03063
Fabio Viscuso <viscuso.fabio@outlook.it>
2022-09-13 00:28:22 +0200

    updated staging

---
+++ components/navigation/Navbar.tsx
@@ -0,0 +1,50 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+// import translation function
+import { useTranslation, Trans } from 'next-i18next';
+import Image from "next/image";
+
+export default function Navbar() {
+    const router = useRouter()
+    const { t } = useTranslation()
+
+    const onToggleLanguageClick = (newLocale: string) => {
+        const { pathname, asPath, query } = router
+        router.push({ pathname, query }, asPath, { locale: newLocale })
+    }
+    const changeTo = router.locale === 'en' ? 'ita' : 'en'
+
+    return (
+        <nav className="fixed top-2 md:top-10 right-0 flex flex-col gap-5 justify-center md:justify-end z-50">
+            {router.pathname !== '/' &&
+                <Link href='/'>
+                    <button className="w-24 text-lg bg-gradient-to-bl from-[#2dd4bf99] to-[#34d39999] hover:from-[#2dd4bfdd] hover:to-[#34d399dd] px-4 py-2 rounded-l-lg text-gray-800 font-semibold backdrop-blur-sm bg-opacity-50 transition-all">
+                        Home
+                    </button>
+                </Link>
+            }
+            {
+                router.pathname !== '/board' &&
+                <Link href='/board'>
+                    <button className="w-24 text-lg bg-gradient-to-bl from-[#2dd4bf99] to-[#34d39999] hover:from-[#2dd4bfdd] hover:to-[#34d399dd] px-4 py-2 rounded-l-lg text-gray-800 font-semibold backdrop-blur-sm bg-opacity-50 transition-all">
+                        {t('header-postit-board')}
+                    </button>
+                </Link>
+            }
+            {
+                router.pathname === '/board' &&
+                <Link href='/new-postit'>
+                    <button className="w-24 text-lg bg-gradient-to-bl from-[#2dd4bf99] to-[#34d39999] hover:from-[#2dd4bfdd] hover:to-[#34d399dd] px-4 py-2 rounded-l-lg text-gray-800 font-semibold backdrop-blur-sm bg-opacity-50 transition-all">
+                        {t('header-add-postit')}
+                    </button>
+                </Link>
+            }
+            <button onClick={() => onToggleLanguageClick(changeTo)} className="flex flex-col items-center w-24 text-lg bg-gradient-to-bl from-[#2dd4bf99] to-[#34d39999] hover:from-[#2dd4bfdd] hover:to-[#34d399dd] px-4 py-2 rounded-l-lg text-gray-800 font-semibold backdrop-blur-sm bg-opacity-50 transition-all">
+                <span className="text-2xl leading-none">&#8631;</span>
+                {router.locale === 'en' && <Image src={'/icons/flag-it.png'} width={30} height={30} alt={''} />}
+                {router.locale === 'ita' && <Image src={'/icons/flag-gb.png'} width={30} height={30} alt={''} />}
+            </button>
+        </nav>
+    )
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,23 @@
+import { appWithTranslation } from 'next-i18next'
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+import Layout from '../components/ui/Layout'
 import '../styles/global.css'
 
-export default function App({ Component, pageProps }) {
+const App = ({ Component, pageProps }) => {
+    const router = useRouter()
+    const formattedPath = router.pathname.replace(/\//, '').replace(/-/, ' ');
     return (
-        <Component{...pageProps} />
+        <>
+            <Head>
+                <title>
+                    {router.pathname === '/' ? 'Fabio Viscuso: home' : `Fabio Viscuso: ${formattedPath}`}
+                </title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Layout>
+                <Component{...pageProps} />
+            </Layout>
+        </>
     )
 }
