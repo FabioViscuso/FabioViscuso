6340eb40ff032026ca067aae9097db936d29e645
Fabio Viscuso <viscuso.fabio@outlook.it>
2023-07-03 13:29:36 +0200

    update spinner

--- components/navigation/LoadingSpinner.tsx
+++ components/navigation/LoadingSpinner.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div className="fixed top-0 bottom-0 left-0 right-0 backdrop-blur-lg z-40 flex justify-center items-center">
+    <div className="fixed top-0 bottom-0 left-0 right-0 bg-slate-300 bg-opacity-40 backdrop-blur-lg z-40 flex justify-center items-center">
       <div className="lds-ring">
         <div></div>
         <div></div>
