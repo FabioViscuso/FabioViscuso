1dba5fdc2e02a9fa45ddb2edae665ad04ac33c55
Fabio Viscuso <viscuso.fabio@outlook.it>
2023-07-08 10:54:51 +0200

    change homepage headings shadows

--- components/homepage/Heading.tsx
+++ components/homepage/Heading.tsx
@@ -6,7 +6,7 @@ interface Props {
 export default function Heading({ content, additionalCSS }: Props) {
   return (
     <h3
-      className={`text-5xl lg:text-7xl drop-shadow-[0px_0px_1px_rgb(0,0,0)] leading-none ${additionalCSS} `}
+      className={`text-5xl lg:text-7xl [text-shadow:4px_3px_1px_rgba(0,0,0,0.35)] leading-none ${additionalCSS} `}
       data-aos="fade-up"
     >
         {content}
