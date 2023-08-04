05198e45949a30988bebe4eb72ef65eecefa75ee
Fabio Viscuso <viscuso.fabio@outlook.it>
2023-08-04 22:24:13 +0200

    feat: improve navbar with labels to links

--- src/components/navigation/NavigationButton.tsx
+++ src/components/navigation/NavigationButton.tsx
@@ -3,19 +3,25 @@ import Link from "next/link";
 
 interface Props {
   slug: string;
+  label: string;
   content: string | StaticImageData;
 }
 
-export default function NavigationButton({ slug, content }: Props) {
+export default function NavigationButton({ slug, content, label }: Props) {
   return (
-    <Link href={slug}>
-      <button className={`w-16 h-16 text-3xl font-semibold text-gray-800 hover:bg-slate-500 hover:bg-opacity-40 transition-all whitespace-nowrap`}>
+    <Link href={slug} className="group">
+      <button
+        className={`w-16 h-16 text-3xl font-semibold text-gray-800 hover:bg-gray-900 hover:text-gray-200 transition-all whitespace-nowrap`}
+      >
         {typeof content === "string" ? (
           content
         ) : (
-          <Image src={content} alt={`go to ${slug}`} />
+          <Image src={content} alt={`link to ${slug}`} />
         )}
       </button>
+      <span className=" opacity-0 group-hover:opacity-100 transition-all duration-200 absolute left-0 group-hover:-left-2 top-[50%] -translate-y-1/2 -translate-x-full [text-orientation:upright] [writing-mode:vertical-lr] text-sm text-center pointer-events-none ">
+        {label}
+      </span>
     </Link>
   );
 }
